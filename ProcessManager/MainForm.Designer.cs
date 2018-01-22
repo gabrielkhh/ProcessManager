c28ae36023e37d832f1011c95b6473f9277f64f1
unknown <gabrielkok4227@gmail.com>
2018-01-22 10:01:36 +0800

    Initial Commit

---
+++ ProcessManager/MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ProcessManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewProcesses = new System.Windows.Forms.ListView();
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSchedule = new System.Windows.Forms.Button();
+            this.flowLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewProcesses
+            // 
+            this.listViewProcesses.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewProcesses.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName,
+            this.colPID});
+            this.listViewProcesses.Location = new System.Drawing.Point(0, 39);
+            this.listViewProcesses.Name = "listViewProcesses";
+            this.listViewProcesses.Size = new System.Drawing.Size(394, 285);
+            this.listViewProcesses.TabIndex = 0;
+            this.listViewProcesses.UseCompatibleStateImageBehavior = false;
+            this.listViewProcesses.View = System.Windows.Forms.View.Details;
+            // 
+            // colName
+            // 
+            this.colName.Text = "Process Name";
+            this.colName.Width = 170;
+            // 
+            // colPID
+            // 
+            this.colPID.Text = "PID";
+            this.colPID.Width = 168;
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Controls.Add(this.btnSchedule);
+            this.flowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(394, 39);
+            this.flowLayoutPanel.TabIndex = 1;
+            // 
+            // btnSchedule
+            // 
+            this.btnSchedule.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnSchedule.Location = new System.Drawing.Point(3, 3);
+            this.btnSchedule.Name = "btnSchedule";
+            this.btnSchedule.Size = new System.Drawing.Size(75, 30);
+            this.btnSchedule.TabIndex = 0;
+            this.btnSchedule.Text = "Schedule";
+            this.btnSchedule.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 325);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Controls.Add(this.listViewProcesses);
+            this.Name = "MainForm";
+            this.Text = "Process Manager";
+            this.flowLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewProcesses;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Button btnSchedule;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.ColumnHeader colPID;
+    }
+}
+
---
+++ ProcessManager/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProcessManager
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadProcesses();
+
+            //Associating UI Components with the respective event method.
+            //btnSchedule.Click += new EventHandler(ButtonSchedule_Click);
+        }
+
+        private void LoadProcesses()
+        {
+            // Get all processes running on the local computer.
+            Process[] localAll = Process.GetProcesses();
+            ListViewItem item;
+            ListViewItem.ListViewSubItem[] subItems;
+            foreach (var process in localAll)
+            {
+                //var driveImage = listViewImageList.Images.IndexOfKey(attr);
+                item = new ListViewItem(process.ProcessName);
+                subItems = new ListViewItem.ListViewSubItem[]
+                          {new ListViewItem.ListViewSubItem(item, process.Id.ToString())
+                              //new ListViewItem.ListViewSubItem(item,
+                              //dirInfo.LastAccessTime.ToShortDateString())
+                          };
+                item.SubItems.AddRange(subItems);
+                //item.Tag = dirInfo;
+                listViewProcesses.Items.Add(item);
+            }
+        }
+    }
+}
